e6a369881ac803ac79e2823c356105de08c4f217
Fayaz <fayaz.sd@frontlinesedutech.com>
2025-02-11 20:59:30 +0530

    Developed bye feauture

--- GitHubProject/src/com/Test.java
+++ GitHubProject/src/com/Test.java
@@ -3,12 +3,15 @@ package com;
 public class Test {
 	
 	public static void main(String[] args) {
-		
 		System.out.println("I am Manager...");
+	}
+	
+	public static void sayBye() {
+		System.out.println("Byee. This is developer 2");
 	}
 	
 	public static void sayHi() {
 		System.out.println("Hii This is developer 1");
 	}
-
+	
 }
